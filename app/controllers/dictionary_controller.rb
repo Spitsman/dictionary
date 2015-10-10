422c6eeab0eacef7d10c63942fa592da4f28045d
Spitsman <sanspits@yandex.ru>
2015-10-10 13:37:41 +0300

    commit

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -1,39 +1,39 @@
 class DictionaryController < ApplicationController
 
-	def index
-	end
+  def index
+  end
 
-	def get_langs
-		begin
-			@langs = dictionary.get_langs
-		rescue Exception => error
-			render :text => error.message
-		end
-	end
+  def get_langs
+    begin
+      @langs = dictionary.get_langs
+    rescue Exception => error
+      render :text => error.message
+    end
+  end
 
-	def lookup
-		begin
-			@articles = dictionary.lookup(params[:lang], params[:text])
-		rescue Exception => error
-			render :text => error.message
-		else
-			Request.create(lang: params[:lang],text: params[:text])
-		end		
-	end
+  def lookup
+    begin
+      @articles = dictionary.lookup(params[:lang], params[:text])
+    rescue Exception => error
+      render :text => error.message
+    else
+      Request.create(lang: params[:lang],text: params[:text])
+    end   
+  end
 
-	def journal
-		@requests = Request.all
-	end
+  def journal
+    @requests = Request.all
+  end
 
-	def destroy
-		Request.destroy_all
-		redirect_to action: "index"
-	end
+  def destroy
+    Request.destroy_all
+    redirect_to action: "index"
+  end
 
-	protected
+  protected
 
-	def dictionary
-		@dictionary ||= DictionaryAPI::DictionaryAPI.new('dict.1.1.20150814T100205Z.a0d27651d642b1d1.93ce6ba9cd891aada1fb98d47b6cd89c15a32f2e')
-	end
+  def dictionary
+    @dictionary ||= DictionaryAPI::DictionaryAPI.new('dict.1.1.20150814T100205Z.a0d27651d642b1d1.93ce6ba9cd891aada1fb98d47b6cd89c15a32f2e')
+  end
 
 end
