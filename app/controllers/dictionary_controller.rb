395be29f57fe240a782ec6ae9ae69cc24d9b1160
Spitsman <sanspits@yandex.ru>
2015-10-12 17:03:19 +0300

    initial commit

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -1,13 +1,12 @@
 class DictionaryController < ApplicationController
 
   def index
-  end
-
-  def get_langs
     begin
       @langs = dictionary.get_langs
     rescue Exception => error
       render :text => error.message
+    else
+      @last = self.last_requests
     end
   end
 
@@ -17,7 +16,7 @@ class DictionaryController < ApplicationController
     rescue Exception => error
       render :text => error.message
     else
-      Request.create(lang: params[:lang],text: params[:text])
+      Request.create(lang: params[:lang],text: params[:text].gsub("%20", " "))
     end   
   end
 
@@ -31,6 +30,19 @@ class DictionaryController < ApplicationController
   end
 
   protected
+
+  def last_requests
+    size = Request.all.size
+    last = []
+    if size < 10
+      last = Request.all
+    else
+      for i in size - 10 ... size
+        last << Request.all[i]
+      end
+    end
+    last
+  end
 
   def dictionary
     @dictionary ||= DictionaryAPI::DictionaryAPI.new('dict.1.1.20150814T100205Z.a0d27651d642b1d1.93ce6ba9cd891aada1fb98d47b6cd89c15a32f2e')
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'dictionary#index'
  	get "index" => 'dictionary#index'
-  get "get_langs" => "dictionary#get_langs"
-  get "lookup" => "dictionary#lookup"
+  post "lookup" => "dictionary#lookup"
   get "journal" => "dictionary#journal"
   delete "journal/destroy" => "dictionary#destroy"
 end
