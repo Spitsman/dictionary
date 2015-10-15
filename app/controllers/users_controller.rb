4cbb90784a99e55d1cebddfebf28498a034d7375
Spitsman <sanspits@yandex.ru>
2015-10-15 23:18:59 +0300

    add password confirm and unique user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
 
 	def create 
 	  @user = User.new(user_params)
-	  if @user.save
+	  if @user.valid? && password_confirmed? && unique_user?
+	  	@user.save
 	    session[:user_id] = @user.id 
-	    redirect_to '/' 
+	    redirect_to '/index' 
 	  else 
 	    redirect_to '/signup' 
 	  end 
@@ -18,6 +19,14 @@ class UsersController < ApplicationController
 
 	def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def unique_user? 	
+  	!User.exists?(username: params[:user][:username])
+  end
+
+  def password_confirmed?
+  	params[:user][:password] == params[:user][:confirm_password]
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	has_secure_password 
+	validates :password, { length: { minimum: 6 } }
+	validates :username, { length: { minimum: 4, maximum: 25 } }
 end
