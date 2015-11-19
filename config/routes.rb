8174f51e6f9b83b55f7066801a40e2b74f851d90
Spitsman <sanspits@yandex.ru>
2015-11-20 00:33:55 +0300

    test heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   delete "journal/destroy" => "journal#destroy", as: :destroy_journal
 
   get 'login' => 'sessions#new', as: :login
-  post "login" => "sessions#create"
+  post 'login' => "sessions#create"
   delete 'logout' => 'sessions#destroy', as: :logout
 
   get 'profile' => 'users#profile'
   get 'users/show' => 'users#show', as: :show_users
-  get "signup" => "users#new", as: :signup
+  get 'signup' => "users#new", as: :signup
   delete 'destroy' => 'users#destroy', as: :destroy_user
   resources :users  
 end
