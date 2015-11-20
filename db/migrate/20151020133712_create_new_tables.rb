17f33cc60c3acfa8b4b2f74fbfe88e09f46f1548
Spitsman <sanspits@yandex.ru>
2015-11-20 18:57:51 +0300

    fix migrations

--- db/migrate/20151020133712_create_new_tables.rb
+++ db/migrate/20151020133712_create_new_tables.rb
@@ -2,7 +2,12 @@ class CreateNewTables < ActiveRecord::Migration
   def change
    create_table :users do |t|
     	t.string :username
-    	t.string :password_digest
+    	t.string :crypted_password
+      t.string :role
+      t.string :email
+      t.string :persistence_token
+      t.string :password_salt
+      t.string :
       t.timestamps null: false
     end
 
@@ -19,6 +24,18 @@ class CreateNewTables < ActiveRecord::Migration
     	t.text :position
     	t.text :transcription
       t.timestamps null: false
+    end
+
+    create_table :translation_articles do |t|
+      t.integer :article_id
+      t.text :text
+      t.text :position
+      t.text :gender
+      t.text :animated
+      t.text :synonym
+      t.text :meaning
+      t.text :example
+      t.text :aspect
     end
   end
 end
--- db/migrate/20151103175808_add_columns_to_user.rb
+++ db/migrate/20151103175808_add_columns_to_user.rb
@@ -1,8 +1,5 @@
 class AddColumnsToUser < ActiveRecord::Migration
   def change
-  	add_column :users, :email, :string
-  	add_column :users, :persistence_token, :string
-  	add_column :users, :password_salt, :string
   	add_index :users, :username, unique: true
   end
 end
